29ab913e36fa094f28149e8e5209eaa62dedbea6
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-01-27 21:05:09 +0900

    Time: 51 ms (59.04%), Space: 13.1 MB (89.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,10 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        nums = [(i, nums[i]) for i in range(len(nums))]
+        nums = sorted(nums, key=lambda x:x[1])
         
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        l, r = 0, len(nums) - 1
+        while l < r:
+            if nums[l][1] + nums[r][1] > target:
+                r -= 1
+            elif nums[l][1] + nums[r][1] < target:
+                l += 1
+            else:
+                return [nums[l][0], nums[r][0]]
 
         
