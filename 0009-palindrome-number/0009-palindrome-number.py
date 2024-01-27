62007f2e7c843159b4ae114d72f22fa66072c334
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-01-27 21:13:50 +0900

    Time: 51 ms (31.45%), Space: 11.5 MB (99.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,11 +4,19 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        x = list(str(x))
-        while x and len(x) != 1:
-            if x.pop(0) != x.pop(-1):
-                return False
-        return True
-            
-            
+        # 반자르고 짝수인 경우는 그대로, 홀수인 경우는 중간 숫자 제거
+        # 앞에 잘라진 것들은 스택으로 쌓고
+        # 나머지는 for문 돌면서 스택 pop하여 비교
+        # 만약 pop한 갚과 다르면 false
+        # 다 돌았을 때 true
         
+        x = list(str(x))
+        l, r = x[:len(x)/2], x[len(x)/2:]
+        if len(l) != len(r):
+            r.pop(0)
+        
+        r.reverse()
+        if l == r:
+            return True
+        
+        return False
