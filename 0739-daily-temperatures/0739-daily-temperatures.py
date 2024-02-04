867b7fd59ff8a5a9e4549ab552aa46785a6ae2e8
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-02-04 16:10:40 +0900

    Time: 1054 ms (39.52%), Space: 26.4 MB (52.11%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -5,19 +5,12 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        answer = []
+        answer = [0] * len(temperatures)
         stack = [] # [(idx, temperature)]
-        for idx, val in enumerate(temperatures):
-            while stack and stack[-1][1] < val:
-                s = stack.pop()
-                answer.append((s[0], idx - s[0]))
-            stack.append((idx, val))
-        
-        # stack에 남은 온도는 따뜻해지는 날을 찾지 못한 것이므로 0으로 표기
-        while stack:
-            s = stack.pop()
-            answer.append((s[0], 0))
-        
-        # index 번호로 정렬
-        sorted_answer = sorted(answer, key=lambda x: x[0])
-        return [answer[1] for answer in sorted_answer]
+        for curr_day, curr_temp in enumerate(temperatures):
+            while stack and stack[-1][1] < curr_temp:
+                prev_day, _ = stack.pop()
+                answer[prev_day] = curr_day - prev_day
+            stack.append((curr_day, curr_temp))
+
+        return answer
