0d977b88f444b3cc8bf44a7df6cd2257cb1a7e94
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-03-01 02:09:25 +0900

    Time: 42 ms (20.60%), Space: 11.9 MB (53.30%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,19 +1,16 @@
 class Solution(object):
     def __init__(self):
-        self.min_cost = {}
+        self.min_cost = {0:0, 1:0}
         
     def minCostClimbingStairs(self, cost):
         """
         :type cost: List[int]
         :rtype: int
         """
-        for i in range(len(cost) + 1):
-            if i == 0 or i == 1:
-                self.min_cost[i] = 0
-            else:
-                self.min_cost[i] = min(
-                    self.min_cost[i-1] + cost[i-1],
-                    self.min_cost[i-2] + cost[i-2]
-                )
+        for i in range(2, len(cost) + 1):
+            self.min_cost[i] = min(
+                self.min_cost[i-1] + cost[i-1],
+                self.min_cost[i-2] + cost[i-2]
+            )
         
         return self.min_cost[len(cost)]
