02b8060e5898e68ee5d6f9e7e27412f38b552e15
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-03-02 21:47:57 +0900

    Time: 16 ms (48.50%), Space: 11.6 MB (65.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,20 +1,21 @@
 class Solution(object):
+    def __init__(self):
+        self.unique_paths = {}
+    
+    def dfs(self, row, col):
+            if row <= 0 or col <= 0:
+                return 1
+
+            if (row, col) not in self.unique_paths:
+                self.unique_paths[(row, col)] = self.dfs(row-1, col) + self.dfs(row, col-1)
+
+            return self.unique_paths[(row, col)]
+        
     def uniquePaths(self, m, n):
         """
         :type m: int
         :type n: int
         :rtype: int
         """
-        memo = [[-1] * n for _ in range(m)]
     
-        for r in range(m):
-            memo[r][0] = 1
-
-        for c in range(n):
-            memo[0][c] = 1
-
-        for r in range(1, m):
-            for c in range(1, n):
-                memo[r][c] = memo[r-1][c] + memo[r][c-1]
-
-        return memo[m-1][n-1]
+        return self.dfs(m-1, n-1)
