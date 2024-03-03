6367209cd5874f203cf7a9bf40510c8c29b66a14
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-03-03 16:53:13 +0900

    Time: 43 ms (63.43%), Space: 13.3 MB (32.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,16 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        nums = [(i, nums[i]) for i in range(len(nums))]
-        nums = sorted(nums, key=lambda x:x[1])
+        nums = [(idx, val) for idx, val in enumerate(nums)]
+        nums = sorted(nums, key=lambda x: x[1])
         
-        l, r = 0, len(nums) - 1
-        while l < r:
-            if nums[l][1] + nums[r][1] > target:
-                r -= 1
-            elif nums[l][1] + nums[r][1] < target:
-                l += 1
-            else:
-                return [nums[l][0], nums[r][0]]
-
+        left, right = [0, len(nums) - 1]
+        while left < right:
+            curr_sum = nums[left][1] + nums[right][1]
+            if curr_sum == target:
+                return [nums[left][0], nums[right][0]]
+            
+            elif curr_sum > target:
+                right -= 1
+            elif curr_sum < target:
+                left += 1
+                
+        return []
         
