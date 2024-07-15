3c92cf8eeedb76ad9ca43c295744b8b0fbbb60a3
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-07-15 18:07:26 +0900

    Time: 30 ms (50.92%), Space: 12.3 MB (78.08%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target: # Search to left
+                right = mid - 1
+            elif nums[mid] < target: # Search to right 
+                left = mid + 1   
+            else: # Correct
+                return mid
+            
+        # Incorrect
+        return left 
