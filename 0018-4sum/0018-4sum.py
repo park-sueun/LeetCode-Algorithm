ae340e2313258c3fbaffb384acbfd2a94dfeeb12
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-08-16 17:44:39 +0900

    Time: 894 ms (19.05%), Space: 11.6 MB (76.64%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -6,24 +6,21 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        size = len(nums)
-        res = []
-        for i in range(size-3):
-            if i==0 or (i>0 and nums[i] !=nums[i-1]):
-                for j in range(i+1,size-2):
-                    if j == i+1 or (j > i+1 and nums[j] != nums[j-1]) :
-                        lo,hi,ts = j+1,size-1,target-(nums[i] +nums[j])
-                        while lo < hi :
-                            if nums[lo] + nums[hi] == ts :
-                                res.append([nums[i],nums[j],nums[lo],nums[hi]])
-                                while lo <hi and nums[lo] == nums[lo+1] :
-                                    lo+=1
-                                while lo < hi and nums[hi] == nums[hi-1] : 
-                                    hi-=1
-                                lo+=1
-                                hi-=1
-                            elif nums[lo] + nums[hi] < ts :
-                                lo+=1
-                            else:
-                                hi-=1
+        res = set()
+
+        for i in range(len(nums)-3):
+            for j in range(i+1, len(nums) -2):
+                
+                l, r = j+1, len(nums) -1
+                check = target - nums[i] - nums[j]
+                
+                while l < r:
+                    if nums[l] + nums[r] == check:
+                        res.add((nums[i], nums[j], nums[l], nums[r]))
+                    
+                    if nums[l] + nums[r] > check:
+                        r -= 1
+                    else: #nums[l] + nums[r] < check:
+                        l += 1
+        
         return res
