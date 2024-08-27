1940cfcdc3aa5d0e65be074e014cd5091796bcaa
Park, Su Eun <60188351+park-sueun@users.noreply.github.com>
2024-08-27 17:15:57 +0900

    Time: 98 ms (35.49%), Space: 13.2 MB (64.19%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        answer = 0
+        max_height = max(height)
+        max_place = height.index(max_height)
+        
+        left_max = height[0]
+        right_max = height[-1]
+        
+        # left 돌기
+        for i in range(max_place):
+            left_max = max(left_max, height[i])
+            answer += left_max - height[i]
+        
+        # right 돌기
+        for i in reversed(range(max_place, len(height))):
+            right_max = max(right_max, height[i])      
+            answer += right_max - height[i]      
+        
+        return answer
